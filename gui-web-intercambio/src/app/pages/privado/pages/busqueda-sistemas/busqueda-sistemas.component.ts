022c6d48a4f6a22c19bbd141d4b5eb0190838600
josesaldanapeople <jose.saldana@people-media.com.mx>
2025-12-03 00:06:31 -0600

    cambios busqueda sistemas

--- gui-web-intercambio/src/app/pages/privado/pages/busqueda-sistemas/busqueda-sistemas.component.ts
+++ gui-web-intercambio/src/app/pages/privado/pages/busqueda-sistemas/busqueda-sistemas.component.ts
@@ -1,8 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TablaPrincipalComponent } from '@pages/privado/shared/tabla-principal/tabla-principal.component';
+import { ButtonModule } from 'primeng/button';
+import { Card } from 'primeng/card';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { InputText } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { PopoverModule } from 'primeng/popover';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-busqueda-sistemas',
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    Card,
+    SelectModule,
+    InputText,
+    TableModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    PaginatorModule,
+    PopoverModule,TablaPrincipalComponent],
   templateUrl: './busqueda-sistemas.component.html',
   styleUrl: './busqueda-sistemas.component.scss'
 })
