f52ccbc4cd7e9850738e72b62ba6b7204746a6af
josesaldanapeople <jose.saldana@people-media.com.mx>
2025-12-03 11:02:40 -0600

    maquetacion pantalla busqueda de sistemas

---
+++ gui-web-intercambio/src/app/pages/privado/shared/tabla-acordeon/tabla-acordeon.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ColumnDefinition } from '@models/columa-tabla';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { ButtonModule } from 'primeng/button';
+import { Card } from 'primeng/card';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { InputText } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { PopoverModule } from 'primeng/popover';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
+
+@Component({
+  selector: 'app-tabla-acordeon',
+  standalone: true,
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    Card,
+    SelectModule,
+    InputText,
+    TableModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    PaginatorModule,
+    PopoverModule,
+    NgbAccordionModule,FormsModule
+  ],
+  templateUrl: './tabla-acordeon.component.html',
+  styleUrl: './tabla-acordeon.component.scss'
+})
+export class TablaAcordeonComponent {
+@Input() titulo: string = '';
+ @Input() data: any[] = [];
+  
+
+  @Input() rows: number = 10;
+  @Input() first: number = 0;
+  @Input() total: number = 0;
+
+  @Output() pageChange = new EventEmitter<any>();
+  @Output() checkboxChanged = new EventEmitter<{ row: any, column: ColumnDefinition }>();
+   onPageChange(event: any) {
+    this.pageChange.emit(event);
+   
+}
+ ngOnInit() {
+  console.log('total',this.total);
+ }
+}
