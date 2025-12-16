7ee0ec9af20af44bca7125f53805ec77c29aa6ac
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-12-16 10:13:15 -0600

    ajustes de css por url

--- gui-web-intercambio/src/app/pages/privado/privado-routing.module.ts
+++ gui-web-intercambio/src/app/pages/privado/privado-routing.module.ts
@@ -9,11 +9,11 @@ import {VerificacionDocumentosComponent} from './pages/verificacion-documentos/v
 import {NAV} from '@utils/url-global';
 
 import {documentacionAspiranteResolver} from '../../core/resolvers/documentacion-aspirante.resolver';
-import { verficacionDocsResolver } from '../../core/resolvers/verificacion-docs.resolver';
+import {verficacionDocsResolver} from '../../core/resolvers/verificacion-docs.resolver';
 
-import { ConsultaAntecedentesComponent } from './pages/consulta-antecedentes/consulta-antecedentes.component';
-import { DetalleAntecedentesComponent } from './pages/detalle-antecedentes/detalle-antecedentes.component';
-import { BusquedaSistemasComponent } from './pages/busqueda-sistemas/busqueda-sistemas.component';
+import {ConsultaAntecedentesComponent} from './pages/consulta-antecedentes/consulta-antecedentes.component';
+import {DetalleAntecedentesComponent} from './pages/detalle-antecedentes/detalle-antecedentes.component';
+import {BusquedaSistemasComponent} from './pages/busqueda-sistemas/busqueda-sistemas.component';
 
 const routes: Routes = [{
   path: '',
@@ -37,11 +37,7 @@ const routes: Routes = [{
     {
       path: `${NAV.detalleAntecedentes}/:id`,
       component: DetalleAntecedentesComponent,
-
-
     },
-
-
   ]
 }];
 
