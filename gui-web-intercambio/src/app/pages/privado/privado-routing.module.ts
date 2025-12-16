5637487ccd9eb93e0796eddfee574a17e87d433f
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-12-16 10:27:42 -0600

    ajustes por url

--- gui-web-intercambio/src/app/pages/privado/privado-routing.module.ts
+++ gui-web-intercambio/src/app/pages/privado/privado-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [{
 
     },
     {
-      path: NAV.busquedasistema,
+      path: `${NAV.busquedasistema}/:expediente`,
       component: BusquedaSistemasComponent,
     },
 
