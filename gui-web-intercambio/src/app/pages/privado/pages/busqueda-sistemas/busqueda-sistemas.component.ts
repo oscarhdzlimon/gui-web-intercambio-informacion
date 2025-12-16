bed054cd22124a339c5459144bb171da48d715e9
epina-gdc <eduardo.gomez@people-media.com.mx>
2025-12-15 22:48:03 -0600

    ajustes filtros busqueda sistema

--- gui-web-intercambio/src/app/pages/privado/pages/busqueda-sistemas/busqueda-sistemas.component.ts
+++ gui-web-intercambio/src/app/pages/privado/pages/busqueda-sistemas/busqueda-sistemas.component.ts
@@ -1,19 +1,21 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { GeneralComponent } from '@components/general.component';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { TablaAcordeonComponent } from '@pages/privado/shared/tabla-acordeon/tabla-acordeon.component';
-import { TablaPrincipalComponent } from '@pages/privado/shared/tabla-principal/tabla-principal.component';
-import { ButtonModule } from 'primeng/button';
-import { Card } from 'primeng/card';
-import { ConfirmPopupModule } from 'primeng/confirmpopup';
-import { InputText } from 'primeng/inputtext';
-import { PaginatorModule } from 'primeng/paginator';
-import { PopoverModule } from 'primeng/popover';
-import { SelectModule } from 'primeng/select';
-import { TableModule } from 'primeng/table';
+import {CommonModule} from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {GeneralComponent} from '@components/general.component';
+import {NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
+import {TablaAcordeonComponent} from '@pages/privado/shared/tabla-acordeon/tabla-acordeon.component';
+import {TablaPrincipalComponent} from '@pages/privado/shared/tabla-principal/tabla-principal.component';
+import {ButtonModule} from 'primeng/button';
+import {Card} from 'primeng/card';
+import {ConfirmPopupModule} from 'primeng/confirmpopup';
+import {InputText} from 'primeng/inputtext';
+import {PaginatorModule} from 'primeng/paginator';
+import {PopoverModule} from 'primeng/popover';
+import {SelectModule} from 'primeng/select';
+import {TableModule} from 'primeng/table';
 import {TIPO_CONSULTA_ANTECEDENTES} from '@utils/constants';
+import {TipoDropdown} from '@models/tipo-dropdown.interface';
+import {AntecedentesService} from '@services/antecedentes.service';
 
 @Component({
   selector: 'app-busqueda-sistemas',
@@ -26,23 +28,49 @@ import {TIPO_CONSULTA_ANTECEDENTES} from '@utils/constants';
     ButtonModule,
     ConfirmPopupModule,
     PaginatorModule,
-    PopoverModule,TablaAcordeonComponent,NgbAccordionModule],
+    PopoverModule, TablaAcordeonComponent, NgbAccordionModule],
   templateUrl: './busqueda-sistemas.component.html',
   styleUrl: './busqueda-sistemas.component.scss'
 })
 export class BusquedaSistemasComponent extends GeneralComponent implements OnInit {
-   tituloTabla: string = '';
-   data:any[] = [
-  ];
+  tituloTabla: string = '';
+  data: any[] = [];
   totalRecords: number = 0;
-   data2:any[] = [
-  ];
+  data2: any[] = [];
   totalRecords2: number = 0;
-
-  data3:any[] = [
-  ];
+  data3: any[] = [];
   totalRecords3: number = 0;
 
+  antecedentesService: AntecedentesService = inject(AntecedentesService);
+
+  filtroResultado: TipoDropdown[] = [];
+  nombres: TipoDropdown[] = [];
+  nss: TipoDropdown[] = [];
+
+  filtroForm!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    super();
+    this.obtenerExpediente();
+    this.filtroForm = this.inicializarFiltroForm();
+  }
+
+  inicializarFiltroForm(): FormGroup {
+    return this.fb.group({
+      filtro: ['', Validators.required],
+      valor: [{value: null, disabled: true}]
+    });
+  }
+
+  obtenerExpediente() {
+    this.antecedentesService.getExpediente('CC.NL.-0621/2016').subscribe({
+      next: (respuesta) => {
+        console.log(respuesta);
+      },
+      error: (error) => {
+      }
+    })
+  }
 
   ngOnInit(): void {
     this.inicializatablaNSS1();
@@ -51,37 +79,110 @@ export class BusquedaSistemasComponent extends GeneralComponent implements OnIni
     console.log("init");
   }
 
-  inicializatablaNSS1(){
+  inicializatablaNSS1() {
     this.data = [
-  { asociar: false,nss:"17482569321",nombre: "Ricardo", apaterno: "Palma", amaterno:"García",gestion:1,queja:0,inconformidades:0,amparo:1,procedimiento:0,juicio:1} ,
-  { asociar: false,nss:"17482569321",nombre: "Ricardo", apaterno: "Palma", amaterno:"López",gestion:2,queja:1,inconformidades:0,amparo:1,procedimiento:0,juicio:1} ,
-];
-this.tituloTabla = 'Resultados de la búsqueda por NSS: 17482569321';
-this.totalRecords = this.data.length;
-}
+      {
+        asociar: false,
+        nss: "17482569321",
+        nombre: "Ricardo",
+        apaterno: "Palma",
+        amaterno: "García",
+        gestion: 1,
+        queja: 0,
+        inconformidades: 0,
+        amparo: 1,
+        procedimiento: 0,
+        juicio: 1
+      },
+      {
+        asociar: false,
+        nss: "17482569321",
+        nombre: "Ricardo",
+        apaterno: "Palma",
+        amaterno: "López",
+        gestion: 2,
+        queja: 1,
+        inconformidades: 0,
+        amparo: 1,
+        procedimiento: 0,
+        juicio: 1
+      },
+    ];
+    this.tituloTabla = 'Resultados de la búsqueda por NSS: 17482569321';
+    this.totalRecords = this.data.length;
+  }
 
- inicializatablaNSS2(){
+  inicializatablaNSS2() {
     this.data2 = [
-  { asociar: false,nss:"17482569321",nombre: "Ricardo", apaterno: "Palma", amaterno:"García",gestion:1,queja:0,inconformidades:0,amparo:1,procedimiento:0,juicio:1} ,
-];
-this.tituloTabla = 'Resultados de la búsqueda por NSS: 17482569321';
-this.totalRecords2 = this.data2.length;
-}
+      {
+        asociar: false,
+        nss: "17482569321",
+        nombre: "Ricardo",
+        apaterno: "Palma",
+        amaterno: "García",
+        gestion: 1,
+        queja: 0,
+        inconformidades: 0,
+        amparo: 1,
+        procedimiento: 0,
+        juicio: 1
+      },
+    ];
+    this.tituloTabla = 'Resultados de la búsqueda por NSS: 17482569321';
+    this.totalRecords2 = this.data2.length;
+  }
 
-inicializatabla3(){
+  inicializatabla3() {
     this.data3 = [
-  { asociar: false,nss:"17482569321",nombre: "Ricardo", apaterno: "Palma", amaterno:"García",gestion:1,queja:0,inconformidades:0,amparo:1,procedimiento:0,juicio:1} ,
-  { asociar: false,nss:"17482569321",nombre: "Ricardo", apaterno: "Palma", amaterno:"López",gestion:2,queja:1,inconformidades:0,amparo:1,procedimiento:0,juicio:1} ,
-  { asociar: false,nss:"17482569321",nombre: "Ricardo", apaterno: "Palma", amaterno:"Hernández",gestion:2,queja:1,inconformidades:0,amparo:1,procedimiento:0,juicio:1} ,
-];
-this.tituloTabla = 'Resultados de la búsqueda por NSS: 17482569321';
-this.totalRecords3 = this.data3.length;
-}
+      {
+        asociar: false,
+        nss: "17482569321",
+        nombre: "Ricardo",
+        apaterno: "Palma",
+        amaterno: "García",
+        gestion: 1,
+        queja: 0,
+        inconformidades: 0,
+        amparo: 1,
+        procedimiento: 0,
+        juicio: 1
+      },
+      {
+        asociar: false,
+        nss: "17482569321",
+        nombre: "Ricardo",
+        apaterno: "Palma",
+        amaterno: "López",
+        gestion: 2,
+        queja: 1,
+        inconformidades: 0,
+        amparo: 1,
+        procedimiento: 0,
+        juicio: 1
+      },
+      {
+        asociar: false,
+        nss: "17482569321",
+        nombre: "Ricardo",
+        apaterno: "Palma",
+        amaterno: "Hernández",
+        gestion: 2,
+        queja: 1,
+        inconformidades: 0,
+        amparo: 1,
+        procedimiento: 0,
+        juicio: 1
+      },
+    ];
+    this.tituloTabla = 'Resultados de la búsqueda por NSS: 17482569321';
+    this.totalRecords3 = this.data3.length;
+  }
 
 
-   cargarPagina(event: any) {
+  cargarPagina(event: any) {
     console.log("Paginación:", event);
   }
+
   cambiarEstado(event: any) {
     console.log("Checkbox cambiado:", event);
   }
